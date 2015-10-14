8279e4f0fe427dc472f5b0d38b0cc104d3b6c4a5
zlito <zitojc99@gmail.com>
2015-10-13 19:27:21 -0600

    Database account

--- ITTCheckBook/app/src/main/java/com/example/zito/ittcheckbook/MainActivity.java
+++ ITTCheckBook/app/src/main/java/com/example/zito/ittcheckbook/MainActivity.java
@@ -1,18 +1,89 @@
 package com.example.zito.ittcheckbook;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    EditText acctNumber, firstName, lastName, bankName, bankBalance, txtDate, txtNotes;
+    Button btnAdd, btnDelete, btnUpdate;
+    Zacct_Helper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // *** Display the Account Info screen  *****
+        acctNumber = (EditText) findViewById(R.id.acctNumber);
+        firstName = (EditText) findViewById(R.id.firstName);
+        lastName = (EditText) findViewById(R.id.lastName);
+        bankName = (EditText) findViewById(R.id.bankName);
+        bankBalance = (EditText) findViewById(R.id.bankBalance);
+        txtDate = (EditText) findViewById(R.id.txtDate);
+        txtNotes = (EditText) findViewById(R.id.txtNotes);
+        // *** Button on the screen
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+
+        db = new Zacct_Helper(this);
+
+        // *** cALL THE LISTENERS  *****
+        btnAdd.setOnClickListener(this);
+        btnUpdate.setOnClickListener(this);
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == btnAdd) {
+            insert();
+            clearText();
+
+        }
+        if (v==btnUpdate)
+        {
+
+        }
+    }
+
+    private void insert()
+    {
+        //String zaccount = acctNumber.getText().toString();
+        String actnumber = acctNumber.getText().toString().trim();
+        String fname = firstName.getText().toString().trim();
+        String lname = lastName.getText().toString().trim();
+        String bkname = bankName.getText().toString().trim();
+        String bkbalance = bankBalance.getText().toString().trim();
+        String actdate = txtDate.getText().toString().trim();
+        String actnotes = txtNotes.getText().toString().trim();
+        db.addAccount(actnumber, fname, lname, bkname, bkbalance, actdate, actnotes);
+        Toast.makeText(this, "The Account was created !", Toast.LENGTH_SHORT).show();
+
+    }
+
+    //** Clear fields on screen
+    public void clearText()
+    {
+        acctNumber.setText("");
+        firstName.setText("");
+        lastName.setText("");
+        bankName.setText("");
+        bankBalance.setText("");
+        txtDate.setText("");
+        txtNotes.setText("");
+    }
+}
+
+
+// *****  Code below do not apply for the current project **********
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -35,3 +106,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+*/
---
+++ ITTCheckBook/app/src/main/java/com/example/zito/ittcheckbook/Zacct_Helper.java
@@ -0,0 +1,80 @@
+package com.example.zito.ittcheckbook;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Zito on 10/13/2015.
+ */
+public class Zacct_Helper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "account_DB";
+    public static final String TABLE_NAME = "account";
+    public static final String ACCT_NUMBER = "acctNumber";
+    public static final String FIRST_NAME = "firstName";
+    public static final String LAST_NAME = "lastName";
+    public static final String BANK_NAME = "bankName";
+    public static final String BANK_BALANCE = "bankBalance";
+    public static final String ACCT_DATE = "acctDate";
+    public static final String ACCT_NOTES = "acctNotes";
+
+    private static final int DB_VERSION = 1;
+
+    public Zacct_Helper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    // Calling the constructor
+    public Zacct_Helper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    // Creating the account table
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE " + TABLE_NAME
+                + "(" + ACCT_NUMBER + " INTEGER PRIMARY KEY,"
+                + FIRST_NAME + " TEXT," + LAST_NAME + " TEXT," + BANK_NAME + " TEXT,"
+                + BANK_BALANCE + " DOUBLE," + ACCT_DATE + " TEXT," + ACCT_NOTES + " TEXT);";
+
+        db.execSQL(sql);
+
+    }
+
+    // Delete the table and ceate a new table - if needed
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = ("DROP TABLE IF EXISTS account");
+        db.execSQL(sql);
+        onCreate(db);
+    }
+
+        // Enter records
+    public boolean addAccount(String actnumber, String fname, String lname, String bkname, String bkbalance, String actdate, String actnotes) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ACCT_NUMBER, actnumber);
+        contentValues.put(FIRST_NAME, fname);
+        contentValues.put(LAST_NAME, lname);
+        contentValues.put(BANK_NAME, bkname);
+        contentValues.put(BANK_BALANCE, bkbalance);
+        contentValues.put(ACCT_DATE, actdate);
+        contentValues.put(ACCT_NOTES, actnotes);
+
+        db.insert(TABLE_NAME, null, contentValues);
+        db.close();
+        return true;
+    }
+
+    //Position cursor to start reading records ---> For view records process
+    public Cursor getAccount(int acctNumber) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM account WHERE acctNumber="+ acctNumber + "", null);
+        return c;
+
+
+    }
+}
